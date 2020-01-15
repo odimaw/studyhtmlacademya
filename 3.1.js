2b4697897071b5eb47facdbbe96d880de075dbb3
Дима <odimaw@yandex.ru>
2020-01-15 18:45:35 +0300

    Начали вести истоию

---
+++ 3.1.js
@@ -0,0 +1,67 @@
+"use strict"
+
+alert('1223454321');
+
+// class Car {
+//     constructor(speed) {
+//         this.speed = speed;
+//     }
+//     viewSpeed() {
+
+//         return 'Моя скорость ' + this.speed + ' км/ч';
+//     }
+// }
+
+// let audi = new Car(200);
+// document.querySelector('[id="car"]').innerText = audi.viewSpeed();
+
+// audi.speed = 300;
+// document.querySelector('[id="car2"]').innerText = audi.viewSpeed();
+
+
+
+// console.log(document.getElementById('header'));
+
+// console.log(typeof document.getElementById('header'));
+
+// document.getElementById('header').innerText = ')))))';
+
+// console.log(document.getElementById('header'));
+
+
+
+// class Hater {
+//     constructor(firdtName, lastName) {
+//        this.firdtName = firdtName;
+//         this.lastName = lastName;
+//     }
+//     sayName() {
+//         console.log(`${this.firdtName} ${this.lastName}`);
+//     }
+// }
+
+// let hater1 = new Hater('Норман','Озборн');
+// hater1['sayName']();
+
+
+// let drivers = ['Ivan', 'Sergey'];
+
+// console.log(drivers.length);
+// console.log(drivers);
+
+
+
+
+// let car = {
+//     color: 'red',
+//     open() {
+//         alert('12345');
+//     }
+// }
+
+// car.open();
+
+
+
+// let anonym = () => alert('12345');
+// anonym();
